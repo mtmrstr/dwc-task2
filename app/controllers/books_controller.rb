7a2112ae5ea58da69cb6d10d036a09c7917c03bb
mtmrstr <106470191+mtmrstr@users.noreply.github.com>
2022-06-15 12:17:49 +0000

    {add}4-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,16 @@ class BooksController < ApplicationController
   end
   def create
     @book=Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:success] = 'Book was successfully created.'
+    redirect_to book_path(@book)
+    else
+    @books=Book.all
+    render:index
+    end
   end
+
+
   def show
     @book =Book.find(params[:id])
   end
@@ -18,12 +25,17 @@ class BooksController < ApplicationController
   end
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
+    if  @book.update(book_params)
+      flash[:success] = 'Book was successfully updated.'
     redirect_to book_path(@book.id)
+    else
+
+      render:edit
+    end
   end
   def destroy
    @book=Book.find(params[:id])
-   book.destroy
+   @book.destroy
    redirect_to '/books'
   end
   private
